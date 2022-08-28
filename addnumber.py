fb6096c32d333d710a70f2ec2d8a8eb13fc5f295
imtiyaz hasan <imtiyaz.hasan@ymail.com>
2022-08-28 18:08:28 +0530

    addnumber

---
+++ addnumber.py
@@ -0,0 +1,12 @@
+
+###create file with addnumber.py and run the command python3 addnumber.py. Put below content into the file
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+

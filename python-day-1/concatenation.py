1044739e0698276e56eae6c3e9a837d7efe07d92
imtiyaz hasan <imtiyaz.hasan@ymail.com>
2022-08-30 00:04:07 +0530

    add new program

---
+++ python-day-1/concatenation.py
@@ -0,0 +1,10 @@
+##these are examples of concatenations where two strings can be added together 
+print ("Missile Man of India" + " - Sir APJ Abdul Kalam")
+
+print ('Technology is' + '- a Future')
+
+print ("python version is" + " 3.10")
+
+print ("python version is" + str(3.100))
+
+print ("3.4" + "3.3")
---
+++ python-day-1/stringrepetition.py
@@ -0,0 +1,7 @@
+##Example to repeat the string
+print ("hello\t\n" * 5)
+###convert the float value in Integer
+print ("hello\t" * int(5.0))
+
+print ("###It can take only Integer not float value. it will give below error")
+print ("hello\t" * 5.0)
